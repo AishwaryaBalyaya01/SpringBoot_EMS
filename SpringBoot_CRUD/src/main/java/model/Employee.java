d0111cbabe44a6bf88a6a02bda47b080c9ab17c0
AishwaryaBalyaya01 <Aishwarya.balyaya19@gmail.com>
2023-06-22 20:20:10 -0400

    Annotations

---
+++ SpringBoot_CRUD/src/main/java/model/Employee.java
@@ -0,0 +1,28 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="employees")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="firstName")
+	private String firstName;
+	
+	@Column(name="lastName")
+	private String lastName;
+	
+	@Column(name="emailId")
+	private String emailId;
+}
